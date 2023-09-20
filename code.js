6c3556ac9b420b4e75ff9be86e106cdd83ac4f6a
ksbleek <kyle.s.bleeker@gmail.com>
2023-09-19 19:58:31 -0500

    Finished tests exercise but having trouble with add giving NaN.

--- code.js
+++ code.js
@@ -9,3 +9,28 @@ function sayHello(x){
     }
     return `Hello, ${x}!`
 }
+
+function isFive(input) {
+    return input === 5
+}
+
+function isEven(input){
+    let num = parseFloat(input)
+    if(isNaN(num)){
+        return false
+    } else {
+        return input % 2 === 0;
+    }
+}
+
+function isVowel(input) {
+    return input === 'a' || input === 'A' || input === 'e' || input === 'E' || input === 'i' || input === 'I' || input === 'o' || input === 'O' || input === 'u' || input === 'U';
+}
+
+function add(num1, num2) {
+    const sum = parseFloat(num1) + parseFloat(num2);
+    if (isNaN(sum)) {
+        return NaN;
+    }
+    return sum;
+}
--- tests.js
+++ tests.js
@@ -41,3 +41,95 @@ describe('sayHello', function(){
     });
 
 })
+
+describe('isFive', function() {
+    it('should be a defined function', function () {
+        expect(typeof isFive).toBe('function');
+    });
+    it("returns true when the number is 5", function() {
+        expect(isFive(5)).toBe(true);
+    });
+    it("returns false when the number is not 5", function() {
+        expect(isFive(4)).toBe(false);
+    });
+    it("returns false when the argument is not a number", function() {
+        expect(isFive("5")).toBe(false);
+    });
+})
+
+describe('isEven', function() {
+    it('should be a defined function', function () {
+        expect(typeof isEven).toBe('function');
+    });
+    it("returns true for even numbers", function() {
+        expect(isEven(2)).toBe(true);
+        expect(isEven(-4)).toBe(true);
+        expect(isEven("8")).toBe(true);
+    });
+    it("returns false for odd numbers", function() {
+        expect(isEven(1)).toBe(false);
+        expect(isEven(3)).toBe(false);
+    });
+    it("returns false for non-numeric strings", function() {
+        expect(isEven("banana")).toBe(false);
+    });
+    it("returns false when called with Infinity", function() {
+        expect(isEven(Infinity)).toBe(false);
+    });
+    it("returns false when called with a Boolean", function() {
+        expect(isEven(true)).toBe(false);
+        expect(isEven(false)).toBe(false);
+    });
+    it("returns false when called without an argument", function() {
+        expect(isEven()).toBe(false);
+    });
+})
+
+describe('isVowel', function() {
+    it('should be a defined function', function () {
+        expect(typeof isVowel).toBe('function');
+    });
+    it('should always return a Boolean', function () {
+        expect(isVowel("a")).toBe(true);
+        expect(isVowel("A")).toBe(true);
+        expect(isVowel("y")).toBe(false);
+        expect(isVowel(4)).toBe(false);
+    });
+    it('returns false when called with a Boolean', function () {
+        expect(isVowel("true")).toBe(false);
+        expect(isVowel("false")).toBe(false);
+    });
+    it('returns false when called with a string of more than one character', function () {
+        expect(isVowel("banana")).toBe(false);
+        expect(isVowel("taco")).toBe(false);
+    });
+    it('returns false when called without an argument', function () {
+        expect(isVowel()).toBe(false);
+    });
+})
+describe('add', function() {
+    it('should be a defined function', function () {
+        expect(typeof add).toBe('function');
+    });
+    it('Adding 2 and 3 equals 5', function () {
+        expect(add(2, 3)).toBe(5);
+    });
+    it('Adding -3 and -9 equals -12', function () {
+        expect(add(-3, -9)).toBe(-12);
+    });
+    it('Adding "5" and 6 equals 11', function () {
+        expect(add("5", 6)).toBe(11);
+    });
+    it('Adding "-4" and "10" equals "6"', function () {
+        expect(add("-4", "10")).toBe(6);
+    });
+    it('Adding "banana" and "split" equals NaN', function () {
+        expect(add("banana", "split")).toBe(NaN);
+    });
+    it('Adding 2 and "apples" equals NaN', function () {
+        expect(add(2, "apples")).toBe(NaN);
+    });
+    it('Giving no argument', function () {
+        expect(add()).toBe(NaN);
+    });
+})
